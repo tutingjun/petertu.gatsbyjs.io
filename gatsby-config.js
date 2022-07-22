23633e065f0c86db3af1313f2cbbba982a495ae6
tutingjun <50512161+tutingjun@users.noreply.github.com>
2022-07-22 14:43:47 -0500

    play with images, mdx and blog css

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,33 @@ module.exports = {
         path: `${__dirname}/blog`,
       },
     },
-    "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        root: __dirname,
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 800,
+              linkImagesToOriginal: false,
+              backgroundColor: "transparent",
+            },
+          },
+        ],
+      },
+    },
     "gatsby-transformer-sharp",
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
   ],
 };
